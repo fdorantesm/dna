58f06b8fae292c08ffb027bbf83cca13c1aa4e12
Fernando Dorantes <fernando@morada.mx>
2022-03-03 16:48:26 -0600

    Add ConfigModule to the app

---
+++ src/app/config/types/config.type.ts
@@ -0,0 +1,5 @@
+import { ServerConfiguration } from './server.type';
+
+export type ConfigLoader = {
+  server: ServerConfiguration;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import * as volleyball from 'volleyball';
+
 import { AppModule } from './app/app.module';
+import { ServerConfiguration } from './app/config/types/server.type';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService: ConfigService = app.get(ConfigService);
+  const config = configService.get<ServerConfiguration>('server');
+
+  const { host, port } = config;
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  app.enableVersioning();
+  app.use(volleyball);
+
+  app.listen(port, host, () => {
+    Logger.log(`Server ready on http://${host}:${port}`, 'Application');
+  });
 }
 bootstrap();
