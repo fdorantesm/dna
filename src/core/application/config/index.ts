071f09381367d7fe3f75365ba77ecfb4a254df50
Fernando Dorantes <fernando@morada.mx>
2022-03-06 22:01:39 -0600

    Add transform interceptor and exception filter

--- src/core/application/config/index.ts
+++ src/core/application/config/index.ts
@@ -1,7 +1,7 @@
 import { ConfigModuleOptions } from '@nestjs/config';
 import * as Joi from 'joi';
 
-import { dnaSchema } from '../../modules/dna/application/config/dna.schema';
+import { dnaSchema } from '../../../modules/dna/application/config/dna.schema';
 import { serverSchema } from './server/server.schema';
 import { serverConfig } from './server/config';
 import { databaseSchema } from './database/database.schema';
